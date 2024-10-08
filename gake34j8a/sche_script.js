c5d5c0465b055266a921352e5058ccd9f4c30213
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2024-10-08 15:06:07 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -23,7 +23,7 @@ const musicGuid =[
     "musicname": "ShowChampion",
     "musicEP": "532회",
     "musictime": "17:00KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8">Kim Bomin, MADEIN, RESCENE, YENA, POW, ASC2NT, XODIAC, The Wind, VANNER, P1Harmony, Yoon Seobin</font>`
   },
   {
     "mair": "nomal",
