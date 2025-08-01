bca679a881fdc7ae49786136296fa29faa1f142c
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2025-08-02 05:22:57 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -40,9 +40,9 @@ const musicGuid =[
     "date": "Fri.",
     "musiconoroff" : true,
     "musicname": "Music Bank",
-    "musicEP": "1259회",
+    "musicEP": "1260회",
     "musictime": "17:00KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8">CrazAngel, SAY MY NAME, ICHILLIN', STAYC, YENA, ASC2NT, Su Hyun Shin, PARK SANG MIN, ONE PACT, SEVENTOEIGHT, XngHan&Xoul, THE BOYZ, NAM WOO HYUN, ARrC, EPEX, JAY, TOMORROW X TOGETHER</font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
   },
   {
     "mair": "nomal",
@@ -50,7 +50,7 @@ const musicGuid =[
     "musiconoroff" : true,
     "musicname": "Music Core",
     "musicEP": "910회",
-    "musictime": "15:15KST",
+    "musictime": "15:05KST",
     "musicsetlist":`ShowGuest:<font color="#adadb8">cosmosy, Ryu Yeon Ju, SAY MY NAME, ICHILLIN', STAYC, YENA, ASC2NT, ARrC, Su Hyun Shin, NAM WOO HYUN, EPEX, Dragon Pony, XngHan&Xoul, JAY, THE BOYZ, TOMORROW X TOGETHER</font>`
   },
 
@@ -140,6 +140,7 @@ function setup(){
     //setTimeout(function(){document.getElementById("ad_info").innerHTML =``;},60000);
 	document.getElementById("livechat").innerHTML =`<iframe src="https://www.twitch.tv/embed/rollelmoberry/chat?darkpopout&parent=Kenterzone.github.io" style="border: 0;height: 85%;width:100%;" frameborder="no" ></iframe>`;
 }
+
 
 
 
