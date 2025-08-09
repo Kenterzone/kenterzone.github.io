d8b0242b7ec7828ed4bdb7f8c2ebe779515eed95
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2025-08-09 18:02:03 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -40,9 +40,9 @@ const musicGuid =[
     "date": "Fri.",
     "musiconoroff" : true,
     "musicname": "Music Bank",
-    "musicEP": "1260회",
+    "musicEP": "1261회",
     "musictime": "17:00KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8">Son E Ji U, ifeye, KiiiKiii, Queenz Eye, ICHILLIN', SAY MY NAME, Yves, YENA, KOYOTE, IDID, ONE PACT, SEVENTOEIGHT, CLOSE YOUR EYES, N.TOP, ASC2NT, Dragon Pony, EPEX, EVNNE, 20th Century Boy’z,</font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
   },
   {
     "mair": "nomal",
@@ -140,6 +140,7 @@ function setup(){
     //setTimeout(function(){document.getElementById("ad_info").innerHTML =``;},60000);
 	document.getElementById("livechat").innerHTML =`<iframe src="https://www.twitch.tv/embed/rollelmoberry/chat?darkpopout&parent=Kenterzone.github.io" style="border: 0;height: 85%;width:100%;" frameborder="no" ></iframe>`;
 }
+
 
 
 
