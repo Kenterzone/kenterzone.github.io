6e89c96d6ff0775c660615001aa164ed85811260
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2025-08-29 17:56:45 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -61,7 +61,7 @@ const musicGuid =[
     "musicname": "Inkigayo",
     "musicEP": "1280회",
     "musictime": "15:25KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8">JO JUNGMIN, iii, AtHeart, MADEIN S, Queenz Eye, RAINBOW18, JUNIEL, Kep1er, IVE, JOY, JEON SOMI, SUNMI, idntt, CORTIS, AMPERS&ONE, 8TURN, AB6IX, TEEN TOP, Stray Kids</font>`
   },  ////SP_GUIDE/////
 
 {
@@ -140,6 +140,7 @@ function setup(){
     //setTimeout(function(){document.getElementById("ad_info").innerHTML =``;},60000);
 	document.getElementById("livechat").innerHTML =`<iframe src="https://www.twitch.tv/embed/rollelmoberry/chat?darkpopout&parent=Kenterzone.github.io" style="border: 0;height: 85%;width:100%;" frameborder="no" ></iframe>`;
 }
+
 
 
 
