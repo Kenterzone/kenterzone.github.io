1527c939ef7675a1017b40689e8f2d4d7a8a9eeb
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2025-09-12 18:15:17 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -61,7 +61,7 @@ const musicGuid =[
     "musicname": "Inkigayo",
     "musicEP": "1282회",
     "musictime": "15:25KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8">Gavy NJ, KAYA, MOMOLAND, CHAEYOUNG(TWICE), DAYOUNG, WENDY, CMDM, AxMxP, CORTIS, HAECHAN, ALL(H)OURS, LIM YOUNG WOONG, CIX, NCT WISH, JUNG DAE HYUN, ZEROBASEONE, MONSTA X</font>`
   },  ////SP_GUIDE/////
 
 {
@@ -140,6 +140,7 @@ function setup(){
     //setTimeout(function(){document.getElementById("ad_info").innerHTML =``;},60000);
 	document.getElementById("livechat").innerHTML =`<iframe src="https://www.twitch.tv/embed/rollelmoberry/chat?darkpopout&parent=Kenterzone.github.io" style="border: 0;height: 85%;width:100%;" frameborder="no" ></iframe>`;
 }
+
 
 
 
