7e39c9dd01bbd6cdc3d9a01536c6363b520eed67
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2025-09-18 02:23:39 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -21,9 +21,9 @@ const musicGuid =[
     "date": "Wed.",
     "musiconoroff" : true,
     "musicname": "ShowChampion",
-    "musicEP": "569회",
+    "musicEP": "570회",
     "musictime": "17:00KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8">WiTCHX, Gavy NJ, DAYOUNG, MOMOLAND, DNA, 2Z + plomo, ALL(H)OURS, AxMxP, CIX, JUNHEE, Sentimental Squad, YU HYUN WOO, Jung Dae Hyun</font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
   },
   {
     "mair": "nomal",
@@ -32,7 +32,7 @@ const musicGuid =[
     "musicname": "M!CountDown",
     "musicEP": "906회",
     "musictime": "18:08KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8">SOORIN, YOUNGBIN, DAYOUNG, BADVILLAIN, PRIMROSE, YUQI, IDID, AxMxP, ALL(H)OURS, LUN8, CORTIS, BOYS ll PLANET <Love Is>, CIX, LIM YOUNG WOONG, Jang Wooyoung, JUNHEE, HAECHAN</font>`
   },
 
   {
@@ -140,6 +140,7 @@ function setup(){
     //setTimeout(function(){document.getElementById("ad_info").innerHTML =``;},60000);
 	document.getElementById("livechat").innerHTML =`<iframe src="https://www.twitch.tv/embed/rollelmoberry/chat?darkpopout&parent=Kenterzone.github.io" style="border: 0;height: 85%;width:100%;" frameborder="no" ></iframe>`;
 }
+
 
 
 
