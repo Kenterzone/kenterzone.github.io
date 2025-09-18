2a1331338550a38fbd13606e8bb3f488bc36f633
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2025-09-18 17:30:52 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -42,7 +42,7 @@ const musicGuid =[
     "musicname": "Music Bank",
     "musicEP": "1265회",
     "musictime": "17:00KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8">SOORIN, YOUNGBIN, Dahye, DAYOUNG, PRIMROSE, BADVILLAIN, YUQI, AxMxP, CORTIS, IDID, JUNG DAE HYUN, HAECHAN, ALL(H)OURS, CIX, LUN8, JUNHEE, LIM YOUNG WOONG, Jang Wooyoung</font>`
   },
   {
     "mair": "nomal",
@@ -51,7 +51,7 @@ const musicGuid =[
     "musicname": "Music Core",
     "musicEP": "917회",
     "musictime": "15:15KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8">Chae EunJung x Haeun, YOUNGBIN, BADVILLAIN, PRIMROSE, DAYOUNG, YUQI, CORTIS, AxMxP, IDID, CIX, KIM HEEJAE, PLAVE, LUN8, ALL(H)OURS, TREASURE, Jang Wooyoung, HAECHAN, JUNHEE</font>`
   },
 
   {
@@ -140,6 +140,7 @@ function setup(){
     //setTimeout(function(){document.getElementById("ad_info").innerHTML =``;},60000);
 	document.getElementById("livechat").innerHTML =`<iframe src="https://www.twitch.tv/embed/rollelmoberry/chat?darkpopout&parent=Kenterzone.github.io" style="border: 0;height: 85%;width:100%;" frameborder="no" ></iframe>`;
 }
+
 
 
 
