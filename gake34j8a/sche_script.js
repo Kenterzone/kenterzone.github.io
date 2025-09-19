3daefe0b5c991a0aa8b4c0b89f1d05343ea116df
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2025-09-19 16:02:33 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -61,7 +61,7 @@ const musicGuid =[
     "musicname": "Inkigayo",
     "musicEP": "1283회",
     "musictime": "15:25KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8">DAYOUNG, SOORIN, YOUNGBIN, BADVILLAIN, Se Yeon Yun, PRIMROSE, YUQI, IDID, AxMxP, ALL(H)OURS, LUN8, CORTIS, CIX, JUNG DAE HYUN, JUNHEE, TREASURE, HAECHAN, Jang Wooyoung(feat. DAMINI)</font>`
   },  ////SP_GUIDE/////
 
 {
@@ -140,6 +140,7 @@ function setup(){
     //setTimeout(function(){document.getElementById("ad_info").innerHTML =``;},60000);
 	document.getElementById("livechat").innerHTML =`<iframe src="https://www.twitch.tv/embed/rollelmoberry/chat?darkpopout&parent=Kenterzone.github.io" style="border: 0;height: 85%;width:100%;" frameborder="no" ></iframe>`;
 }
+
 
 
 
