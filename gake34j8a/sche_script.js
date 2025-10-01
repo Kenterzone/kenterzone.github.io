7931a3369be06f01216cd5774bfa7d9a0bfe5350
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2025-10-02 00:07:19 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -32,7 +32,7 @@ const musicGuid =[
     "musicname": "M!CountDown",
     "musicEP": "908회",
     "musictime": "18:08KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8">YOUNGBIN, Hearts2Hearts, izna, cosmosy, SUHO, STEAL HEART CLUB, IDID, AxMxP, LEE JUN YOUNG, KickFlip, X, SUHO, Ha Hyun Sang, The KingDom, BLUHWA, CxM(SEVENTEEN)</font>`
   },
 
   {
@@ -49,9 +49,9 @@ const musicGuid =[
     "date": "Sat.",
     "musiconoroff" : true,
     "musicname": "Music Core",
-    "musicEP": "918회",
+    "musicEP": "919회",
     "musictime": "15:15KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8">YOUNGBIN, Hearts2Hearts, BADVILLAIN, DAYOUNG, LUN8, ALL(H)OURS, CORTIS, AxMxP, IDID, SUHO, CIX, The KingDom, LEE JUN YOUNG, Ham Ji Woo, KickFlip, JUNHEE</font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
   },
 
   {
@@ -59,9 +59,9 @@ const musicGuid =[
     "date": "Sun.",
     "musiconoroff" : true,
     "musicname": "Inkigayo",
-    "musicEP": "1284회",
+    "musicEP": "1285회",
     "musictime": "12:20KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8">YOUNGBIN, GENBLUE, cosmosy, Hearts2Hearts, BADVILLAIN, YUQI, DAYOUNG, The KingDom, LUN8, KickFlip, TIOT, CIX, IDID, ALL(H)OURS, LEE JUN YOUNG, CORTIS, SUHO</font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
   },  ////SP_GUIDE/////
 
 {
@@ -140,6 +140,7 @@ function setup(){
     //setTimeout(function(){document.getElementById("ad_info").innerHTML =``;},60000);
 	document.getElementById("livechat").innerHTML =`<iframe src="https://www.twitch.tv/embed/rollelmoberry/chat?darkpopout&parent=Kenterzone.github.io" style="border: 0;height: 85%;width:100%;" frameborder="no" ></iframe>`;
 }
+
 
 
 
