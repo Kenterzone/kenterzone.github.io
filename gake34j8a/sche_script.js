bd58d3a2ebdff5f79ba6a58c4c17801a2604dd67
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2025-10-04 04:48:20 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -51,13 +51,13 @@ const musicGuid =[
     "musicname": "Music Core",
     "musicEP": "919회",
     "musictime": "15:15KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8">cosmosy, GENBLUE, Hearts2Hearts, izna, YOUNGBIN, 1000°, CxM(SEVENTEEN), Yoon Seobin, AIMERS, POW, TIOT, Park Sung On, KickFlip, AxMxP, IDID, BLUHWA</font>`
   },
 
   {
     "mair": "nomal",
     "date": "Sun.",
-    "musiconoroff" : true,
+    "musiconoroff" : false,
     "musicname": "Inkigayo",
     "musicEP": "1285회",
     "musictime": "12:20KST",
@@ -140,6 +140,7 @@ function setup(){
     //setTimeout(function(){document.getElementById("ad_info").innerHTML =``;},60000);
 	document.getElementById("livechat").innerHTML =`<iframe src="https://www.twitch.tv/embed/rollelmoberry/chat?darkpopout&parent=Kenterzone.github.io" style="border: 0;height: 85%;width:100%;" frameborder="no" ></iframe>`;
 }
+
 
 
 
