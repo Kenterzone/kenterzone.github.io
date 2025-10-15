fd6604fafd98ebcf7a3730af56381caca0c861c0
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2025-10-15 18:23:16 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -30,9 +30,9 @@ const musicGuid =[
     "date": "Thu.",
     "musiconoroff" : true,
     "musicname": "M!CountDown",
-    "musicEP": "910회",
+    "musicEP": "909회",
     "musictime": "18:08KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8">HITGS, izna, MEOVV, BABYMONSTER, NMIXX, HWASA, IDID, KickFlip, TIOT, POW, U-KNOW, Hi-Fi Un!corn, Musical <Kinky Boots>, TWS, ONEWE, Pagaehun, BAE JIN YOUNG, BAE173</font>`
   },
 
   {
@@ -140,6 +140,7 @@ function setup(){
     //setTimeout(function(){document.getElementById("ad_info").innerHTML =``;},60000);
 	document.getElementById("livechat").innerHTML =`<iframe src="https://www.twitch.tv/embed/rollelmoberry/chat?darkpopout&parent=Kenterzone.github.io" style="border: 0;height: 85%;width:100%;" frameborder="no" ></iframe>`;
 }
+
 
 
 
