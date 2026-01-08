1c04275659d8579e676fb087e9af9da051610245
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2026-01-08 19:38:12 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -40,7 +40,7 @@ const musicGuid =[
     "date": "Fri.",
     "musiconoroff" : true,
     "musicname": "Music Bank",
-    "musicEP": "1276회",
+    "musicEP": "1275회",
     "musictime": "17:20KST",
     "musicsetlist":`ShowGuest:<font color="#adadb8">GRANADA, SAY MY NAME, H1-KEY, CHUU, Apink, WAKER, BE BOYS, idntt, EUI BIN KWON, Tae Jin Ah, SHIN SOOHYUN</font>`
   },
@@ -141,6 +141,7 @@ function setup(){
     //setTimeout(function(){document.getElementById("ad_info").innerHTML =``;},60000);
 	document.getElementById("livechat").innerHTML =`<iframe src="https://www.twitch.tv/embed/rollelmoberry/chat?darkpopout&parent=Kenterzone.github.io" style="border: 0;height: 85%;width:100%;" frameborder="no" ></iframe>`;
 }
+
 
 
 
