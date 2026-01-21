0ada8f3896d927afdd24c1812875699924f92ea7
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2026-01-21 17:53:54 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -32,7 +32,7 @@ const musicGuid =[
     "musicname": "M!CountDown",
     "musicEP": "913회",
     "musictime": "18:08KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8">)</font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8">dodree, SAY MY NAME, CHUU, LA POEM, LNGSHOT, ALPHA DRIVE ONE, AxMxP, idntt, ONEUS, ONE OR EIGHT, CATCH THE YOUNG, 2Z, TNX, n.SSign, ENHYPEN, EXO</font>`
   },
 
   {
@@ -141,6 +141,7 @@ function setup(){
     //setTimeout(function(){document.getElementById("ad_info").innerHTML =``;},60000);
 	document.getElementById("livechat").innerHTML =`<iframe src="https://www.twitch.tv/embed/rollelmoberry/chat?darkpopout&parent=Kenterzone.github.io" style="border: 0;height: 85%;width:100%;" frameborder="no" ></iframe>`;
 }
+
 
 
 
