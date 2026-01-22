19e99a6d4014b5c717bd66b7e636096501b80186
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2026-01-22 21:31:19 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -21,9 +21,9 @@ const musicGuid =[
     "date": "Wed.",
     "musiconoroff" : true,
     "musicname": "ShowChampion",
-    "musicEP": "583회",
+    "musicEP": "584회",
     "musictime": "17:00KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8">SAY MY NAME, LIGHTSUM, CHUU, idntt, n.SSign, ALPHA DRIVE ONE, IN SEONG, CHOI SUNG YUN(Y), CATCH THE YOUNG</font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
   },
   {
     "mair": "nomal",
@@ -42,7 +42,7 @@ const musicGuid =[
     "musicname": "Music Bank",
     "musicEP": "1277회",
     "musictime": "17:20KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8">dodree, SAY MY NAME, XG, H1-KEY, CHUU, idntt, ONE OR EIGHT, ALPHA DRIVE ONE, AxMxP, LNGSHOT, LA POEM, TNX, n.SSign, CATCH THE YOUNG, ONEUS, ENHYPEN, EXO</font>`
   },
   {
     "mair": "nomal",
@@ -51,7 +51,7 @@ const musicGuid =[
     "musicname": "Music Core",
     "musicEP": "932회",
     "musictime": "15:20KST",
-    "musicsetlist":`ShowGuest:<font color="#adadb8"></font>`
+    "musicsetlist":`ShowGuest:<font color="#adadb8">dodree, SAY MY NAME, XG, CHUU, CATCH THE YOUNG, ONE OR EIGHT, AxMxP, idntt, ALPHA DRIVE ONE, LNGSHOT, ONEUS, LA POEM, ENHYPEN, TNX, CHOI SOO HO, n.SSign, EXO</font>`
   },
 
   {
@@ -141,6 +141,7 @@ function setup(){
     //setTimeout(function(){document.getElementById("ad_info").innerHTML =``;},60000);
 	document.getElementById("livechat").innerHTML =`<iframe src="https://www.twitch.tv/embed/rollelmoberry/chat?darkpopout&parent=Kenterzone.github.io" style="border: 0;height: 85%;width:100%;" frameborder="no" ></iframe>`;
 }
+
 
 
 
