84dd7101f7151a19d2a8201a6806a890316f5ba9
Kenterzone <121766134+Kenterzone@users.noreply.github.com>
2026-01-31 01:29:15 +0800

    Update sche_script.js

--- gake34j8a/sche_script.js
+++ gake34j8a/sche_script.js
@@ -57,7 +57,7 @@ const musicGuid =[
   {
     "mair": "nomal",
     "date": "Sun.",
-    "musiconoroff" : false,
+    "musiconoroff" : true,
     "musicname": "Inkigayo",
     "musicEP": "1297회",
     "musictime": "15:10KST",
@@ -141,6 +141,7 @@ function setup(){
     //setTimeout(function(){document.getElementById("ad_info").innerHTML =``;},60000);
 	document.getElementById("livechat").innerHTML =`<iframe src="https://www.twitch.tv/embed/rollelmoberry/chat?darkpopout&parent=Kenterzone.github.io" style="border: 0;height: 85%;width:100%;" frameborder="no" ></iframe>`;
 }
+
 
 
 
